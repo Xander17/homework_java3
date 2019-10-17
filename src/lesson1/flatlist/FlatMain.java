2710c87f882a85ed95348e2f7d9c8c735df4575c
Cromag <alseko_2001@mail.ru>
2019-10-17 21:40:02 +0300

    toMap method

--- src/lesson1/flatlist/FlatMain.java
+++ src/lesson1/flatlist/FlatMain.java
@@ -16,15 +16,19 @@ public class FlatMain {
         list.add(new Flatlist("Test3"));
         list.add(new Flatlist("Test4"));
 
-        HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+        HashMap<String, ArrayList<Integer>> map = listToMap(list);
 
+        System.out.println(map);
+    }
+
+    private static HashMap<String, ArrayList<Integer>> listToMap(ArrayList<Flatlist> list) {
+        HashMap<String, ArrayList<Integer>> map = new HashMap<>();
         list.forEach(entry -> {
             String k = entry.getName();
             ArrayList<Integer> v = map.getOrDefault(k, new ArrayList<>());
             v.add(entry.getId());
             map.put(k, v);
         });
-
-        System.out.println(map);
+        return map;
     }
 }
