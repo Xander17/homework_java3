332fa96e0790315d237dabf91a19e21b7a8f65e4
Cromag <alseko_2001@mail.ru>
2019-10-25 15:23:15 +0300

    ByteArray

--- src/lesson3/bytearray/ByteArray.java
+++ src/lesson3/bytearray/ByteArray.java
@@ -1,4 +1,19 @@
 package lesson3.bytearray;
 
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class ByteArray {
+    public static void main(String[] args) {
+        File file = new File(Paths.get("src/lesson3/bytearray/file.txt").toString());
+        byte[] bytes = new byte[512];
+        try (FileInputStream in = new FileInputStream(file)) {
+            while (in.read(bytes) != -1) {
+                System.out.println(Arrays.toString(bytes));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
