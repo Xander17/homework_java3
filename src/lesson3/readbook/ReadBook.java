09621c9870844cd29856ab2b2aa9e991f5d76e46
Cromag <alseko_2001@mail.ru>
2019-10-25 11:59:13 +0300

    fixes

--- src/lesson3/readbook/ReadBook.java
+++ src/lesson3/readbook/ReadBook.java
@@ -18,13 +18,15 @@ public class ReadBook {
         try (RandomAccessFile in = new RandomAccessFile(file, "r"); Scanner csl = new Scanner(System.in)) {
             getPage(in, pageNum);
             while (true) {
-                System.out.println();
                 System.out.println("--Страница " + pageNum + ". Всего страниц: " + totalPages + ". Введите номер страницы или выберите следующую '>' или предуыдущую '<': ");
                 String pageStr = csl.next();
                 if (pageStr.equalsIgnoreCase("exit")) break;
                 if (pageStr.matches("^\\d+$")) {
-                    pageNum = Integer.parseInt(pageStr);
-                    if (pageNum > 0 && pageNum <= totalPages) getPage(in, pageNum);
+                    int newPage = Integer.parseInt(pageStr);
+                    if (newPage > 0 && newPage <= totalPages) {
+                        pageNum = newPage;
+                        getPage(in, pageNum);
+                    }
                 } else if (pageStr.equals(">") && pageNum < totalPages) getPage(in, ++pageNum);
                 else if (pageStr.equals("<") && pageNum > 1) getPage(in, --pageNum);
             }
@@ -35,24 +37,25 @@ public class ReadBook {
     }
 
     private static void getPage(RandomAccessFile in, int page) throws IOException {
-        in.seek(CHARS_PER_PAGE * (page - 1) + 1);
+        in.seek(CHARS_PER_PAGE * (page - 1));
         byte[] b = new byte[CHARS_PER_PAGE];
         in.read(b, 0, CHARS_PER_PAGE);
         StringBuilder s = new StringBuilder(new String(b, "WINDOWS-1251"));
 
         cutLines(s, CHARS_PER_LINE);
         System.out.println(s.toString());
+        System.out.println();
     }
 
     private static void cutLines(StringBuilder s, int length) {
         int i = 0;
         while (true) {
             int i2 = s.indexOf("\n", i);
-            if (i2 < 0 && s.length() - i < length) break;
-            while ((s.length() - i > length) && s.charAt(i + length) != ' ') i++;
+            if (i2 < 0 && s.length() - i <= length) break;
             if (i2 - i > length || (i2 < 0 && s.length() - i > length)) {
+                while ((s.length() - i > length) && s.charAt(i + length) != ' ') i++;
                 s.insert(i + length, "\r\n");
-                i = i + length + 2;
+                i = i + length + 1;
             } else
                 i = i2 + 1;
         }
