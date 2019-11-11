3f75281934856e7dd8edb40309f570eb11a613f7
Cromag <alseko_2001@mail.ru>
2019-11-11 20:37:30 +0300

    maven

--- src/test/java/lesson6/Task2Test.java
+++ src/test/java/lesson6/Task2Test.java
@@ -1,9 +1,6 @@
 package lesson6;
 
-import javafx.concurrent.Task;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
